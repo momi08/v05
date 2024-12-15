9d76601640b347bf944fcbca672cb81b395c52a5
Tomislav Prstec <toprstec@vsite.hr>
2024-12-15 20:25:18 +0100

    v05 solved

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,41 @@
 
 namespace vsite::oop::v5
 {
+	double operator ""_inch(unsigned long long inch) {
+		return inch * 2.54;
+	}
 
-}
+	double operator ""_eur(unsigned long long eur) {
+		return eur * 7.5345;
+	}
+
+	vehicle::vehicle(int year) {
+		yr = year;
+	}
+
+	tire::tire(double size) {
+		d = size;
+	}
+
+	product::product(double price) {
+		pr = price;
+	}
+
+	car::car(int yearof, double priceof, double size_of_tires) : vehicle(yearof), product(priceof), car_tr(size_of_tires) {}
+
+	int vehicle::year() {
+		return yr;
+	}
+
+	double tire::diameter() {
+		return d;
+	}
+
+	double product::price() {
+		return pr;
+	}
+
+	double car::tire_diameter() {
+		return car_tr.diameter();
+	}
+}	
--- app/app.h
+++ app/app.h
@@ -4,5 +4,34 @@
 
 namespace vsite::oop::v5 
 {
+	double operator ""_inch(unsigned long long inch);
+	double operator ""_eur(unsigned long long eur);
 
+	class tire {
+		double d;
+	public:
+		tire(double d);
+		double diameter();
+	};
+
+	class vehicle {
+		int yr;
+	public:
+		vehicle(int yr);
+		int year();
+	};
+
+	class product {
+		double pr;
+	public:
+		product(double pr);
+		double price();
+	};
+
+	class car : public vehicle, public product {
+		tire car_tr;
+	public:
+		car(int yr, double pr, double car_tr);
+		double tire_diameter();
+	};
 }
